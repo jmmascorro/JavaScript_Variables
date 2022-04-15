5d9c2077a84f1cdafd09b5b25213bb4843423c00
jmmascorro <jessemascorro88@gmail.com>
2022-04-15 16:23:36 -0500

    Completed JavaScript Intro Exercises

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Jesse";
+let lastName = "Mascorro";
+var age = 33;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+let templateFullName = `${firstName} ${lastName}`;
+console.log("fullName: ", fullName);
+console.log("templateFullName: ", templateFullName);
 
 // Exercise 3
+let myStory;
+let city = "Winnsboro";
+let passtime = "playing discgolf";
+
+myStory = `Hello! My name is ${fullName}. I Live in ${city}. I enjoy ${passtime} and coding!`;
+console.log(myStory);
